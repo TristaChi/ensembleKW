07c17de832d8ebae74a6a4395612d4c4cdc7f300
TristaChi <zhangchi.rdfz@gmail.com>
2022-05-25 11:57:39 -0400

    update train.sh

--- train.sh
+++ train.sh
@@ -1,71 +1,104 @@
-#!/bin/bash 
-PYTHONPATH=/nethome/ericwong/convex_adversarial.preview.nips
-# for L2 note that an L-infinity ball with radius eps
-# has approximately the same volume as an L2 ball with radius
-# sqrt(d/pi)*eps, where d is the number of dimensions. 
-# For MNIST this is 
-# sqrt(784/pi)*0.1=1.58
-# sqrt(784/pi)*0.3=4.74
-# and for cifar this is 
-# sqrt(1024/pi)*0.0348=0.628
-# sqrt(1024/pi)*0.139=2.51
+################################################## MNIST l_inf ##################################################
+file=[dir_to_save_models]
 
-# arguments that are universal across all experiments
-cuda_ids=0,1,2,3
-cascade=6
-epochs=60
-schedule_length=20
+####### mnist small exact 1 #######
+name=smallExact_1
+python examples/mnist.py \
+    --epochs 60 \
+    --epsilon 0.1 \
+    --starting_epsilon 0.01 \
+    --schedule_length 20 \
+    --prefix ${file}/${name} \
+    --verbose 200 \
+    --norm_train l1 \
+    --norm_test l1 \
+> ${file}/${name}.out
 
-# L2 ball arguments
-norm_type=l1_median
-norm_eval=l1
-# norm_type=l2_normal
-# norm_eval=l2
+####### mnist small, large, eps=1,3 #######
+model_type=small
+eps=3
+python examples/mnist.py \
+    --epochs 60 \
+    --model ${model_type} \
+    --epsilon 0.${eps} \
+    --starting_epsilon 0.01 \
+    --schedule_length 20 \
+    --proj 50 \
+    --prefix ${file}/${model_type}_${eps} \
+    --verbose 100 \
+    --norm_train l1_median \
+    --norm_test l1 \
+    --print_log False \
+> ${file}/${model_type}_${eps}.out
 
-# MNIST parameters
-prefix="tmp/mnist"
-starting_epsilon=0.01
-parameters="--epochs ${epochs} --starting_epsilon ${starting_epsilon} --schedule_length ${schedule_length} --cascade ${cascade} --prefix ${prefix} --verbose 200 --cuda_ids ${cuda_ids}"
+################################################## MNIST l_2 ##################################################
+####### mnist small exact 158 #######
+name=smallExact_158
+python examples/mnist.py \
+    --epochs 60 \
+    --epsilon 1.58 \
+    --starting_epsilon 0.01 \
+    --schedule_length 20 \
+    --prefix ${file}/${name} \
+    --verbose 200 \
+    --norm_train l2 \
+    --norm_test l2 \
+> ${file}/${name}.out
 
-# [pick an epsilon]
-# linf ball epsilons
-eps=0.1
-# eps=0.3
-# l2 ball epsilons
-# eps=1.58
+####### mnist small, large #######
+model_type=small
+python examples/mnist.py \
+    --epochs 60 \
+    --model ${model_type} \
+    --epsilon 1.58 \
+    --starting_epsilon 0.01 \
+    --schedule_length 20 \
+    --proj 50 \
+    --prefix ${file}/${model_type}_158 \
+    --verbose 100 \
+    --norm_train l2_normal \
+    --norm_test l2 \
+    --print_log False \
+> ${file}/${model_type}_158.out
 
-# small, exact 
-python examples/mnist.py --epsilon ${eps} --norm_train ${norm_eval} --norm_test ${norm_eval} ${parameters}
+################################################## CIFAR l_inf ##################################################
+file=[dir_to_save_models]
 
-# all remaining experiments use an approximation for training with 50 projections
-parameters="--proj 50 --norm_train ${norm_type} ${parameters}"
+####### cifar small, large, epspx=2,8 #######
+model_type=small
+epspx=2
+eps=0.0348 # epspx=2
+# eps=0.139 # epspx=8
+python examples/cifar.py \
+    --epochs 60 \
+    --model ${model_type} \
+    --epsilon ${eps} \
+    --starting_epsilon 0.001 \
+    --schedule_length 20 \
+    --proj 50 \
+    --prefix ${file}/${model_type}_${eps} \
+    --verbose 100 \
+    --norm_train l1_median \
+    --norm_test l1 \
+    --print_log False \
+> ${file}/${model_type}_${eps}.out
 
-# small 
-# python examples/mnist.py --epsilon ${eps} --norm_test ${norm_eval} ${parameters} 
 
-# large
-# python examples/mnist.py --epsilon ${eps} --norm_test ${norm_eval} --test_batch_size 8 --model large ${parameters}
-
-# CIFAR parameters
-prefix="tmp/cifar"
-starting_epsilon=0.001
-parameters="--epochs ${epochs} --starting_epsilon ${starting_epsilon} --schedule_length ${schedule_length} --cascade ${cascade} --prefix ${prefix} --verbose 200 --cuda_ids ${cuda_ids}"
-parameters="--proj 50 --norm_train ${norm_type} ${parameters}"
-
-# [pick an epsilon]
-# linf ball epsilons
-eps=0.0348
-# eps=0.139
-# l2 ball epsilons
-# eps=0.157
-
-# small
-# python examples/cifar.py --epsilon ${eps} --norm_test ${norm_eval} --test_batch_size 25 ${parameters}
-
-# large
-# python examples/cifar.py --epsilon ${eps} --norm_test ${norm_eval} --test_batch_size 8 --model large ${parameters}
-
-# resnet
-# the only model where we defer true (non estimated) test statistics 
-# to after training to save time
-# python examples/cifar.py --epsilon ${eps} --norm_test ${norm_type} --model resnet ${parameters}
+################################################## CIFAR l_2 ##################################################
+####### cifar small, large, epspx=36 #######
+model_type=small
+epspx=36
+eps=0.157
+python examples/cifar.py \
+    --epochs 60 \
+    --model ${model_type} \
+    --epsilon ${eps} \
+    --starting_epsilon 0.001 \
+    --schedule_length 20 \
+    --proj 50 \
+    --prefix ${file}/${model_type}_${eps} \
+    --verbose 100 \
+    --norm_train l2_normal \
+    --norm_test l2 \
+    --print_log False \
+> ${file}/${model_type}_${eps}.out
